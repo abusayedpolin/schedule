463d9131ec4397d7065bf3bda870bf38b12fadce
Polin Khan <55098399+Polinkhan@users.noreply.github.com>
2022-04-04 01:52:07 +0600

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -18,60 +18,72 @@ function msToTime(s) {
   return `${days}D ${hrs}H ${mins}M ${secs + 1}S left`;
 }
 
-toHrsMin = (start, end) => {
-  return time.substring(start, end);
-}
+toHrsMin = (item, start, end) => {
+  return item.time.substring(start, end);
+};
 
-localTime = ()=>{
+localTime = (hrs, min) => {
   let ampm;
-  (hrs > 12)? ampm = "PM" : ampm = "AM";
-  return `${hrs%12}:${min} ${ampm}`;
-}
+  hrs > 12 ? (ampm = "PM") : (ampm = "AM");
+  return `${hrs % 12}:${min} ${ampm}`;
+};
 
-getMonthName = (point)=>{
+getMonthName = (point) => {
   switch (point.getMonth()) {
-    case 0: return "jan";
-    case 1: return "feb";
-    case 2: return "mar";
-    case 3: return "apr";
-    case 4: return "may";
-    case 5: return "jun";
-    case 6: return "jul";
-    case 7: return "aug";
-    case 8: return "sep";
-    case 9: return "oct";
-    case 10: return "nov";
-    case 11: return "dec"; 
+    case 0:
+      return "jan";
+    case 1:
+      return "feb";
+    case 2:
+      return "mar";
+    case 3:
+      return "apr";
+    case 4:
+      return "may";
+    case 5:
+      return "jun";
+    case 6:
+      return "jul";
+    case 7:
+      return "aug";
+    case 8:
+      return "sep";
+    case 9:
+      return "oct";
+    case 10:
+      return "nov";
+    case 11:
+      return "dec";
   }
-}
+};
 
-let sub;
-let date;
-let time;
-
-let day;
-let month;
-let hrs;
-let min;
+let item1 = {
+  sub: "",
+  date: "",
+  time: "",
+};
 
 $(document).ready(function () {
   $("#sub").click(function () {
-    sub = $("#getSub").val();
-    date = $("#getDate").val();
-    time = $("#getTime").val();
-    hrs = toHrsMin(0, 2);
-    min = toHrsMin(3, 5);
-    time = localTime(time);
+    item1.sub = $("#getSub").val();
+    item1.date = $("#getDate").val();
+    item1.time = $("#getTime").val();
 
-    if (sub && date && time) {
+    let hrs = toHrsMin(item1, 0, 2);
+    let min = toHrsMin(item1, 3, 5);
+    item1.time = localTime(hrs, min);
+
+    if (item1.sub && item1.date && item1.time) {
       document.getElementById("wrong").innerText = "";
-      let d = new Date(date);
+      let d = new Date(item1.date);
       d.setHours(hrs);
       d.setMinutes(min);
-      day = d.getDate();
-      month = getMonthName(d);
-      document.getElementById("topic").innerText = sub;
-      document.getElementById("text").innerText =`${day} ${month} at ${time}`;
+      let day = d.getDate();
+      let month = getMonthName(d);
+      document.getElementById("topic").innerText = item1.sub;
+      document.getElementById(
+        "text"
+      ).innerText = `${day} ${month} at ${item1.time}`;
       setInterval(() => {
         document.getElementById("timeLeft").innerText = msToTime(
           d.getTime() - new Date().getTime()
