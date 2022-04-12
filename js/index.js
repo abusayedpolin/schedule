124dd6b1cc9e91ab87712d119ca5a73d463b222e
polinkhan <polinsir@gmail.com>
2022-04-12 06:08:45 +0600

    minor chanes

--- js/index.js
+++ js/index.js
@@ -1,4 +1,18 @@
-import {clock,login} from "./functions.js"
+import { clock, login, eve, hideLogin } from "./functions.js";
 
+eve();
 clock();
-document.querySelector(".login").addEventListener("click",login);
+document.querySelector(".login").addEventListener("click", login);
+document.querySelector(".keybtn").addEventListener("click", () => {
+  let input = document.querySelector(".loginInput");
+  if (input.value) {
+    if (input.value == 64614) {
+      hideLogin();
+    } else {
+      input.value = "wrong!!";
+      setTimeout(() => {
+        input.value = "";
+      }, 1000);
+    }
+  }
+});
