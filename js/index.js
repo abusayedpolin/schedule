196c1b3b1341fb1d69c34c38946d9fb3a5f06be4
polinkhan <polinsir@gmail.com>
2022-04-13 21:40:37 +0600

    error handle

--- js/index.js
+++ js/index.js
@@ -4,6 +4,35 @@ import { pullData } from "./DB.js";
 eve();
 clock();
 
+setTimeout(() => {
+  let data = pullData();
+  let sortData = [];
+  if(data.length == 0){
+    document.querySelector(".error").classList.remove("hide");
+  }
+  while (data.length > 0) {
+    let index = -1;
+    let date = new Date(data[0]["_date"]);
+    date.setTime(setTime(date, data[0]["_time"]));
+
+    data.forEach(function (elem, i) {
+      let cdate = new Date(elem["_date"]);
+      cdate.setTime(setTime(cdate, elem["_time"]));
+
+      if (cdate.getTime() <= date.getTime()) {
+        date = cdate;
+        index = i;
+      }
+    });
+    sortData.push(data[index]);
+    data.splice(index, 1);
+  }
+  sortData.forEach(function (elem, i) {
+    elem.index = i;
+    add(elem);
+  });
+}, 1500);
+
 document.querySelector(".login").addEventListener("click", login);
 document.querySelector(".keybtn").addEventListener("click", () => {
   let input = document.querySelector(".loginInput");
